a13afa2a1a1663c97f8dcb7c47a5530e2a501ca1
Ibrahim <inobani28@gmail.com>
2023-09-30 21:54:50 +0300

    Seeding the Tables with Mock Data

---
+++ LibrarySystem/TablesData/LoansData.sql
@@ -0,0 +1,167 @@
+INSERT INTO [Loans] ([BookID], [BorrowerID], [DateBorrowed], [DueDate], [DateReturned]) 
+VALUES (1, 1, '1/19/2023', '2/19/2023', '02/17/2023');
+
+INSERT INTO [Loans] ([BookID], [BorrowerID], [DateBorrowed], [DueDate], [DateReturned]) 
+VALUES (2, 2, '1/17/2023', '2/17/2023', null);
+
+INSERT INTO [Loans] ([BookID], [BorrowerID], [DateBorrowed], [DueDate], [DateReturned]) 
+VALUES (3, 3, '5/11/2023', '6/11/2023', '06/09/2023');
+
+INSERT INTO [Loans] ([BookID], [BorrowerID], [DateBorrowed], [DueDate], [DateReturned]) 
+VALUES (4, 4, '8/15/2023', '9/15/2023', null);
+
+INSERT INTO [Loans] ([BookID], [BorrowerID], [DateBorrowed], [DueDate], [DateReturned]) 
+VALUES (5, 5, '1/15/2023', '2/15/2023', '02/13/2023');
+
+INSERT INTO [Loans] ([BookID], [BorrowerID], [DateBorrowed], [DueDate], [DateReturned]) 
+VALUES (6, 6, '7/12/2023', '8/12/2023', null);
+
+INSERT INTO [Loans] ([BookID], [BorrowerID], [DateBorrowed], [DueDate], [DateReturned]) 
+VALUES (7, 7, '4/18/2023', '5/18/2023', '05/16/2023');
+
+INSERT INTO [Loans] ([BookID], [BorrowerID], [DateBorrowed], [DueDate], [DateReturned]) 
+VALUES (8, 8, '7/2/2023', '8/2/2023', null);
+
+INSERT INTO [Loans] ([BookID], [BorrowerID], [DateBorrowed], [DueDate], [DateReturned]) 
+VALUES (9, 9, '7/23/2023', '8/23/2023', '08/21/2023');
+
+INSERT INTO [Loans] ([BookID], [BorrowerID], [DateBorrowed], [DueDate], [DateReturned]) 
+VALUES (10, 10, '7/23/2023', '8/23/2023', '08/21/2023');
+
+INSERT INTO [Loans] ([BookID], [BorrowerID], [DateBorrowed], [DueDate], [DateReturned]) 
+VALUES (11, 11, '4/1/2023', '5/1/2023', '04/29/2023');
+
+INSERT INTO [Loans] ([BookID], [BorrowerID], [DateBorrowed], [DueDate], [DateReturned]) 
+VALUES (12, 12, '8/9/2023', '9/9/2023', '09/07/2023');
+
+INSERT INTO [Loans] ([BookID], [BorrowerID], [DateBorrowed], [DueDate], [DateReturned]) 
+VALUES (13, 13, '7/18/2023', '8/18/2023', null);
+
+INSERT INTO [Loans] ([BookID], [BorrowerID], [DateBorrowed], [DueDate], [DateReturned]) 
+VALUES (14, 14, '6/13/2023', '7/13/2023', '07/11/2023');
+
+INSERT INTO [Loans] ([BookID], [BorrowerID], [DateBorrowed], [DueDate], [DateReturned]) 
+VALUES (15, 15, '8/2/2023', '9/2/2023', null);
+
+INSERT INTO [Loans] ([BookID], [BorrowerID], [DateBorrowed], [DueDate], [DateReturned]) 
+VALUES (16, 16, '6/25/2023', '7/25/2023', null);
+
+INSERT INTO [Loans] ([BookID], [BorrowerID], [DateBorrowed], [DueDate], [DateReturned]) 
+VALUES (17, 17, '6/20/2023', '7/20/2023', '07/18/2023');
+
+INSERT INTO [Loans] ([BookID], [BorrowerID], [DateBorrowed], [DueDate], [DateReturned]) 
+VALUES (18, 18, '3/29/2023', '4/29/2023', '04/27/2023');
+
+INSERT INTO [Loans] ([BookID], [BorrowerID], [DateBorrowed], [DueDate], [DateReturned]) 
+VALUES (19, 19, '5/10/2023', '6/10/2023', '06/08/2023');
+
+INSERT INTO [Loans] ([BookID], [BorrowerID], [DateBorrowed], [DueDate], [DateReturned]) 
+VALUES (20, 20, '7/21/2023', '8/21/2023', '08/19/2023');
+
+INSERT INTO [Loans] ([BookID], [BorrowerID], [DateBorrowed], [DueDate], [DateReturned]) 
+VALUES (21, 21, '2/11/2023', '3/11/2023', '03/09/2023');
+
+INSERT INTO [Loans] ([BookID], [BorrowerID], [DateBorrowed], [DueDate], [DateReturned]) 
+VALUES (22, 22, '7/25/2023', '8/25/2023', '08/23/2023');
+
+INSERT INTO [Loans] ([BookID], [BorrowerID], [DateBorrowed], [DueDate], [DateReturned]) 
+VALUES (23, 23, '2/1/2023', '3/1/2023', '02/27/2023');
+
+INSERT INTO [Loans] ([BookID], [BorrowerID], [DateBorrowed], [DueDate], [DateReturned]) 
+VALUES (24, 24, '8/5/2023', '9/5/2023', '09/03/2023');
+
+INSERT INTO [Loans] ([BookID], [BorrowerID], [DateBorrowed], [DueDate], [DateReturned]) 
+VALUES (25, 25, '8/25/2023', '9/25/2023', '09/23/2023');
+
+INSERT INTO [Loans] ([BookID], [BorrowerID], [DateBorrowed], [DueDate], [DateReturned]) 
+VALUES (26, 26, '2/21/2023', '3/21/2023', null);
+
+INSERT INTO [Loans] ([BookID], [BorrowerID], [DateBorrowed], [DueDate], [DateReturned]) 
+VALUES (27, 27, '3/2/2023', '4/2/2023', null);
+
+INSERT INTO [Loans] ([BookID], [BorrowerID], [DateBorrowed], [DueDate], [DateReturned]) 
+VALUES (28, 28, '2/12/2023', '3/12/2023', '03/10/2023');
+
+INSERT INTO [Loans] ([BookID], [BorrowerID], [DateBorrowed], [DueDate], [DateReturned]) 
+VALUES (29, 29, '4/2/2023', '5/2/2023', '04/30/2023');
+
+INSERT INTO [Loans] ([BookID], [BorrowerID], [DateBorrowed], [DueDate], [DateReturned]) 
+VALUES (30, 30, '7/24/2023', '8/24/2023', null);
+
+INSERT INTO [Loans] ([BookID], [BorrowerID], [DateBorrowed], [DueDate], [DateReturned]) 
+VALUES (31, 31, '5/11/2023', '6/11/2023', '06/09/2023');
+
+INSERT INTO [Loans] ([BookID], [BorrowerID], [DateBorrowed], [DueDate], [DateReturned]) 
+VALUES (32, 32, '8/5/2023', '9/5/2023', null);
+
+INSERT INTO [Loans] ([BookID], [BorrowerID], [DateBorrowed], [DueDate], [DateReturned]) 
+VALUES (33, 33, '8/16/2023', '9/16/2023', null);
+
+INSERT INTO [Loans] ([BookID], [BorrowerID], [DateBorrowed], [DueDate], [DateReturned]) 
+VALUES (34, 34, '8/18/2023', '9/18/2023', '09/16/2023');
+
+INSERT INTO [Loans] ([BookID], [BorrowerID], [DateBorrowed], [DueDate], [DateReturned]) 
+VALUES (35, 35, '5/1/2023', '6/1/2023', '05/30/2023');
+
+INSERT INTO [Loans] ([BookID], [BorrowerID], [DateBorrowed], [DueDate], [DateReturned]) 
+VALUES (36, 36, '7/2/2023', '8/2/2023', '07/31/2023');
+
+INSERT INTO [Loans] ([BookID], [BorrowerID], [DateBorrowed], [DueDate], [DateReturned]) 
+VALUES (37, 37, '1/18/2023', '2/18/2023', null);
+
+INSERT INTO [Loans] ([BookID], [BorrowerID], [DateBorrowed], [DueDate], [DateReturned]) 
+VALUES (38, 38, '4/22/2023', '5/22/2023', null);
+
+INSERT INTO [Loans] ([BookID], [BorrowerID], [DateBorrowed], [DueDate], [DateReturned]) 
+VALUES (39, 39, '4/7/2023', '5/7/2023', null);
+
+INSERT INTO [Loans] ([BookID], [BorrowerID], [DateBorrowed], [DueDate], [DateReturned]) 
+VALUES (40, 40, '5/26/2023', '6/26/2023', null);
+
+INSERT INTO [Loans] ([BookID], [BorrowerID], [DateBorrowed], [DueDate], [DateReturned]) 
+VALUES (41, 41, '8/5/2023', '9/5/2023', null);
+
+INSERT INTO [Loans] ([BookID], [BorrowerID], [DateBorrowed], [DueDate], [DateReturned]) 
+VALUES (42, 42, '2/14/2023', '3/14/2023', '03/12/2023');
+
+INSERT INTO [Loans] ([BookID], [BorrowerID], [DateBorrowed], [DueDate], [DateReturned]) 
+VALUES (43, 43, '1/23/2023', '2/23/2023', null);
+
+INSERT INTO [Loans] ([BookID], [BorrowerID], [DateBorrowed], [DueDate], [DateReturned]) 
+VALUES (44, 44, '6/26/2023', '7/26/2023', '07/24/2023');
+
+INSERT INTO [Loans] ([BookID], [BorrowerID], [DateBorrowed], [DueDate], [DateReturned]) 
+VALUES (45, 45, '3/8/2023', '4/8/2023', null);
+
+INSERT INTO [Loans] ([BookID], [BorrowerID], [DateBorrowed], [DueDate], [DateReturned]) 
+VALUES (46, 46, '7/21/2023', '8/21/2023', null);
+
+INSERT INTO [Loans] ([BookID], [BorrowerID], [DateBorrowed], [DueDate], [DateReturned]) 
+VALUES (47, 47, '5/15/2023', '6/15/2023', '06/13/2023');
+
+INSERT INTO [Loans] ([BookID], [BorrowerID], [DateBorrowed], [DueDate], [DateReturned]) 
+VALUES (48, 48, '7/13/2023', '8/13/2023', '08/11/2023');
+
+INSERT INTO [Loans] ([BookID], [BorrowerID], [DateBorrowed], [DueDate], [DateReturned]) 
+VALUES (49, 49, '3/23/2023', '4/23/2023', null);
+
+INSERT INTO [Loans] ([BookID], [BorrowerID], [DateBorrowed], [DueDate], [DateReturned]) 
+VALUES (50, 50, '1/25/2023', '2/25/2023', null);
+
+INSERT INTO [Loans] ([BookID], [BorrowerID], [DateBorrowed], [DueDate], [DateReturned]) 
+VALUES (100, 11, '4/1/2023', '5/1/2023', '04/29/2023');
+
+INSERT INTO [Loans] ([BookID], [BorrowerID], [DateBorrowed], [DueDate], [DateReturned]) 
+VALUES (52, 2, '1/17/2023', '2/17/2023', null);
+
+INSERT INTO [Loans] ([BookID], [BorrowerID], [DateBorrowed], [DueDate], [DateReturned]) 
+VALUES (53, 2, '1/17/2023', '2/17/2023', null);
+
+INSERT INTO [Loans] ([BookID], [BorrowerID], [DateBorrowed], [DueDate], [DateReturned]) 
+VALUES (54, 4, '1/17/2023', '2/17/2023', null);
+
+INSERT INTO [Loans] ([BookID], [BorrowerID], [DateBorrowed], [DueDate], [DateReturned]) 
+VALUES (55, 4, '1/17/2023', '2/17/2023', null);
+
+INSERT INTO [Loans] ([BookID], [BorrowerID], [DateBorrowed], [DueDate], [DateReturned]) 
+VALUES (1, 4, '3/17/2023', '4/17/2023', null);
